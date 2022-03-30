55f1cf71f378a47d083d232f12c3da0994cf429b
Andrei <vitobravis@gmail.com>
2022-03-30 17:41:24 +0700

    webpack config fix

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    mode: "production",
+    entry: "./src/onlyScroll.ts",
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'onlyScroll.js',
+        library: {
+            type: "umd",
+            name: "OnlyScroll",
+            export: "default"
+        },
+    },
+    optimization: {
+        minimize: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+            }
+        ]
+    }
+}
