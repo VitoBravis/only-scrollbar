96d34671be711b99b24dc91329fc86cae1b89819
Andrei <vitobravis@gmail.com>
2023-09-07 15:40:53 +0700

    1.0.2

--- src/onlyScrollbar.d.ts
+++ src/onlyScrollbar.d.ts
@@ -35,7 +35,7 @@ export interface Delta2D {
 /**
  * @description Модификация нативного скрола, работающая по принципу перерасчета текущей позиции с помощью Безье функции.
  * @class
- * @version 1.0.1
+ * @version 1.0.2
  */
 declare class OnlyScrollbar {
     /**
