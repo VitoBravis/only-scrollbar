6955936653b6aae9e960793bf9b9dc7eb4a833f2
Andrei <vitobravis@gmail.com>
2025-08-25 18:28:27 +0700

    docs 2.0 WIP

---
+++ dev/components/example-scroll.ts
@@ -0,0 +1,12 @@
+import {OnlyScrollbarOptions} from "../../src/types";
+
+class ExampleScroll {
+    constructor(
+        public root: HTMLElement,
+        public options: OnlyScrollbarOptions
+    ) {
+
+    }
+}
+
+export default ExampleScroll;
